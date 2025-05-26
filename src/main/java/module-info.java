42e8bec8e1596d50b4a7fc77ae35644633fa238f
Aiarenochka <kovalcukanastasia18@gmail.com>
2025-05-27 02:02:51 +0300

    Term_paper

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module Term.paper {
+    requires com.google.gson;
+    requires lombok;
+    requires spring.beans;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.context;
+    requires spring.web;
+    requires spring.webmvc;
+}
